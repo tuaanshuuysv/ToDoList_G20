c07b26cf6834a30413d9b34e7afe3feb3764b27b
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 12:51:46 +0700

    Implement ArchiveProjectUseCase to archive projects

--- app/src/main/java/com/todolist/app/domain/usecase/project/ArchiveProjectUseCase.java
+++ app/src/main/java/com/todolist/app/domain/usecase/project/ArchiveProjectUseCase.java
@@ -1,5 +1,16 @@
 package com.todolist.app.domain.usecase.project;
 
+import com.todolist.app.data.local.entity.Project;
+import com.todolist.app.data.repository.ProjectRepository;
+
 public class ArchiveProjectUseCase {
-    // TODO: Implement ArchiveProjectUseCase
+    private final ProjectRepository repository;
+    public ArchiveProjectUseCase(ProjectRepository repository) {
+        this.repository = repository;
+    }
+    public void execute(Project project) {
+        project.setArchived(true);
+        project.setUpdatedAt(new java.util.Date());
+        repository.update(project);
+    }
 }
