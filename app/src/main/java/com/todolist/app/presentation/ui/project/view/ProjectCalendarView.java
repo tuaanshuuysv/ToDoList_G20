350fe4740d59178f27219010d529b75770048e58
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 15:35:02 +0700

    Add ProjectCalendarView with task display functionality
    Implemented ProjectCalendarView with RecyclerView to display tasks.

--- app/src/main/java/com/todolist/app/presentation/ui/project/view/ProjectCalendarView.java
+++ app/src/main/java/com/todolist/app/presentation/ui/project/view/ProjectCalendarView.java
@@ -1,5 +1,36 @@
 package com.todolist.app.presentation.ui.project.view;
 
-public class ProjectCalendarView {
-    // TODO: Implement ProjectCalendarView
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.todolist.app.R;
+import com.todolist.app.data.local.entity.Task;
+import com.todolist.app.presentation.ui.home.adapter.TaskAdapter;
+import java.util.List;
+
+public class ProjectCalendarView extends FrameLayout {
+    private RecyclerView recyclerView;
+    private TaskAdapter adapter;
+    public ProjectCalendarView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+    private void init(Context context) {
+        recyclerView = new RecyclerView(context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        adapter = new TaskAdapter();
+        recyclerView.setAdapter(adapter);
+        addView(recyclerView);
+    }
+    public void setTasks(List<Task> tasks) {
+        if (adapter != null) {
+            adapter.setTasks(tasks);
+        }
+    }
 }
