2745fe5f0fac34307f0bf3b452af64e7a17bfcdc
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 12:53:05 +0700

    Implement UpdateProjectUseCase to update project

--- app/src/main/java/com/todolist/app/domain/usecase/project/UpdateProjectUseCase.java
+++ app/src/main/java/com/todolist/app/domain/usecase/project/UpdateProjectUseCase.java
@@ -1,5 +1,16 @@
 package com.todolist.app.domain.usecase.project;
 
+import com.todolist.app.data.local.entity.Project;
+import com.todolist.app.data.repository.ProjectRepository;
+import java.util.Date;
+
 public class UpdateProjectUseCase {
-    // TODO: Implement UpdateProjectUseCase
+    private final ProjectRepository repository;
+    public UpdateProjectUseCase(ProjectRepository repository) {
+        this.repository = repository;
+    }
+    public void execute(Project project) {
+        project.setUpdatedAt(new Date());
+        repository.update(project);
+    }
 }
