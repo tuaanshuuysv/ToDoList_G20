21f5745895cff59d38e8c1e01c3e4147dfbe794c
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 12:52:28 +0700

    Implement DeleteProjectUseCase functionality

--- app/src/main/java/com/todolist/app/domain/usecase/project/DeleteProjectUseCase.java
+++ app/src/main/java/com/todolist/app/domain/usecase/project/DeleteProjectUseCase.java
@@ -1,5 +1,14 @@
 package com.todolist.app.domain.usecase.project;
 
+import com.todolist.app.data.local.entity.Project;
+import com.todolist.app.data.repository.ProjectRepository;
+
 public class DeleteProjectUseCase {
-    // TODO: Implement DeleteProjectUseCase
+    private final ProjectRepository repository;
+    public DeleteProjectUseCase(ProjectRepository repository) {
+        this.repository = repository;
+    }
+    public void execute(Project project) {
+        repository.delete(project);
+    }
 }
