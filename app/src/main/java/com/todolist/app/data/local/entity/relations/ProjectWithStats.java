f424e3931a89bfa7bed04ebe6d889a782b544456
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 12:48:54 +0700

    Implement ProjectWithStats with task statistics
    upload

--- app/src/main/java/com/todolist/app/data/local/entity/relations/ProjectWithStats.java
+++ app/src/main/java/com/todolist/app/data/local/entity/relations/ProjectWithStats.java
@@ -1,5 +1,12 @@
 package com.todolist.app.data.local.entity.relations;
 
+import androidx.room.Embedded;
+import com.todolist.app.data.local.entity.Project;
+
 public class ProjectWithStats {
-    // TODO: Implement ProjectWithStats
+    @Embedded
+    public Project project;
+    public int totalTasks;
+    public int completedTasks;
+    public int pendingTasks;
 }
