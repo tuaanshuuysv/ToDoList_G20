5ea49ccc0a09b7325173697f6bd90c5c76a7a4d3
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 15:35:34 +0700

    Implement ProjectKanbanView as a LinearLayout
    Extend ProjectKanbanView to inherit from LinearLayout and set orientation.

--- app/src/main/java/com/todolist/app/presentation/ui/project/view/ProjectKanbanView.java
+++ app/src/main/java/com/todolist/app/presentation/ui/project/view/ProjectKanbanView.java
@@ -1,5 +1,14 @@
 package com.todolist.app.presentation.ui.project.view;
 
-public class ProjectKanbanView {
-    // TODO: Implement ProjectKanbanView
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ProjectKanbanView extends LinearLayout {
+    public ProjectKanbanView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setOrientation(HORIZONTAL);
+    }
 }
