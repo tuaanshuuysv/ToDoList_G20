53a4bbb66649fb443661548ce40a79d9399041ed
nguyenthaihung123 <h12t2n2005@gmail.com>
2026-01-06 12:49:23 +0700

    Implement ProjectWithTasks with Room annotations
    upload

--- app/src/main/java/com/todolist/app/data/local/entity/relations/ProjectWithTasks.java
+++ app/src/main/java/com/todolist/app/data/local/entity/relations/ProjectWithTasks.java
@@ -1,5 +1,14 @@
 package com.todolist.app.data.local.entity.relations;
 
+import androidx.room.Embedded;
+import androidx.room.Relation;
+import com.todolist.app.data.local.entity.Project;
+import com.todolist.app.data.local.entity.Task;
+import java.util.List;
+
 public class ProjectWithTasks {
-    // TODO: Implement ProjectWithTasks
+    @Embedded
+    public Project project;
+    @Relation(parentColumn = "project_id", entityColumn = "project_id")
+    public List<Task> tasks;
 }
